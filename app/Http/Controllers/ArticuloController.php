19f4a0d090be3a0f1151948912058e05e5eb318a
GIAMPAOLO DELGADO <giampaolo.delgadoo@ug.edu.ec>
2022-02-26 11:16:31 -0500

    Primer Commit

---
+++ app/Http/Controllers/ArticuloController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Articulo;
+use Illuminate\Http\Request;
+
+class ArticuloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articulos=Articulo::all();
+        return \response($articulos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'titulo'=>'required|max:255',
+            'cuerpo'=>'required',
+            'fecha_publicacion'=>'required'
+        ]);
+        $articulo=Articulo::create($request->all());
+        return \response($articulo);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $articulo=Articulo::findOrFail($id);
+        return \response($articulo);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Articulo::findOrFail($id)->update($request->all());
+        return \response("Tarea actualizada");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Articulo::destroy($id);
+        return \response("El artículo con el id: $id ha sido eliminado");
+    }
+}
---
+++ database/seeders/ArticulosSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Articulo;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArticulosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Articulo::factory(50)->create();
+    }
+}
